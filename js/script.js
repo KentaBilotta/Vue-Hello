80b8bf9b8ddbe0090f916fca51bab6380267d3a5
Kenta Bilotta <kentabilotta20@gmail.com>
2022-11-11 19:11:38 +0100

    secondo commit: aggiunta materiale + titolo

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const title = new Vue({
+    el: "#title",
+    data: {
+        message: "Hello World! This title has been written with data (VueJS)"
+    }
+});
+
+const image = new Vue({
+    el: "#image",
+    data: {
+        link: "../sfondo.jpeg"
+    }
+})
