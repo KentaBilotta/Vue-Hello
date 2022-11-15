a5d0fd6efb391e3dd73d7b8e61be206057ae80b8
Kenta Bilotta <kentabilotta20@gmail.com>
2022-11-15 17:11:40 +0100

    inserimento immagine

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
 const title = new Vue({
     el: "#title",
     data: {
-        message: "Hello World! This title has been written with data (VueJS)"
+        message: "Hello World! This title has been written with data (VueJS)",
+        linkImg: "img/sfondo.jpeg"
     }
 });
-
-const image = new Vue({
-    el: "#image",
-    data: {
-        link: "../sfondo.jpeg"
-    }
-})
